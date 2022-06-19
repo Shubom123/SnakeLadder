9c0351f1d397db83c08abe6270158267c1d2c0f2
Shubom123 <shubomnath365@gmail.com>
2022-06-19 17:39:04 +0530

    Added: Dice Roll functionallity added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,8 @@
 public class SnakeLadder{
 public static void main(String args[]){
 System.out.println("Welcome to snake and ladder game");
+int position=0;
+int randomCheck=(int) Math.floor(Math.random() * 10) % 6+1;
+System.out.println(randomCheck);
 }
 }
