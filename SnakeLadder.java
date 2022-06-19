8beb3e9133036b955313c34beb976ff393881a47
Shubom123 <shubomnath365@gmail.com>
2022-06-19 19:48:06 +0530

    Added: Two Player Functionality added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,14 +6,16 @@ public static void main(String args[]){
 System.out.println("Welcome to snake and ladder game");
 int position=0;
 int diceRoll=0;
-
-while(position != destination)
-{
-
-diceRoll++;
+int position2=0;
+int diceRoll2=0;
 int randomCheck=(int) Math.floor(Math.random() * 10) % 6+1;
 int randomCheck1=(int) Math.floor(Math.random() * 10) % 4;
 int randomCheck2=(int) Math.floor(Math.random() * 10) % 20+3;
+
+while(position < destination && position2 < destination)
+{
+
+diceRoll++;
 
 System.out.println(randomCheck);
 switch(randomCheck1){
@@ -49,8 +51,52 @@ System.out.println("You came back to start.");
 else {
 System.out.println("New Position: " + position);
 }
+
+diceRoll2++;
+
+System.out.println(randomCheck);
+switch(randomCheck1){
+case 0:
+System.out.println("No play");
+position2 += 0;
+break;
+case 1:
+System.out.println("Ladder");
+position2 += randomCheck2;
+break;
+case 2:
+System.out.println("Snake");
+position2 -= randomCheck2;
+break;
+case 3:
+System.out.println("Normal");
+position2 += randomCheck;
+break;
 }
-System.out.println("Your winning position: " + position);
-System.out.println("Number of time dicerolled: " + diceRoll);
+if (position2 == 100) {
+break;
+}
+else if (position2 > 100) {
+position2 -= randomCheck;
+System.out.println("Dice Face is invalid so, stay on same position.");
+}
+else if (position2 < 0) {
+position2 = source;
+System.out.println("You came back to start.");
+}
+else {
+System.out.println("New Position: " + position2);
+}
+}
+System.out.println("Player1's position is : "+position);
+System.out.println("Number of times game played : "+diceRoll);
+System.out.println("Player2's position is : "+position2);
+System.out.println("Number of times game played : "+diceRoll2);
+if (position==100) {
+System.out.println("Player1 has Won");
+}
+else {
+System.out.println("Player2 has Won");
+}
 }
 }
