17551ec2a6ce86882521f67f13253a0ce1b4abfd
Shubom123 <shubomnath365@gmail.com>
2022-06-19 18:48:43 +0530

    Added: Reachin winning position functionality added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,10 +1,20 @@
 public class SnakeLadder{
+final static int source = 0;
+final  static  int destination = 100;
+
 public static void main(String args[]){
 System.out.println("Welcome to snake and ladder game");
 int position=0;
+int diceRoll=0;
+
+while(position != destination)
+{
+
+diceRoll++;
 int randomCheck=(int) Math.floor(Math.random() * 10) % 6+1;
 int randomCheck1=(int) Math.floor(Math.random() * 10) % 3;
 int randomCheck2=(int) Math.floor(Math.random() * 10) % 20+3;
+
 System.out.println(randomCheck);
 switch(randomCheck1){
 case 0:
@@ -20,5 +30,21 @@ System.out.println("Snake");
 position -= randomCheck2;
 break;
 }
+if (position == 100) {
+break;
+}
+else if (position > 100) {
+position -= randomCheck;
+System.out.println("Dice Face is invalid so, stay on same position.");
+}
+else if (position < 0) {
+position = source;
+System.out.println("You came back to start.");
+}
+else {
+System.out.println("New Position: " + position);
+}
+}
+System.out.println("You won");
 }
 }
